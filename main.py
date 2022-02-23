5f5bf28c71200d8d78a333326b870bf5bf362bd2
sfif7890 <dvornikov.sashakpc1882020@gmail.com>
2022-02-23 22:14:35 +0400

    towers

--- main.py
+++ main.py
@@ -33,31 +33,27 @@ while True:
 print("\033[38m{}".format("█"),"\033[0m{}".format("█"),"\033[39m{}".format("█"))
 
 def kadr():
-    for k in range(15):
+    for i in range(15):
         print("")
-    for o in range(rings):
-        stroka = " "
-        for p in range(towers):
-            kolco = ""
-            color = pole[p][o]%9
-            if color == 0:
-                kolco = '\033[30m{}'.format("█")*pole[p][o] + '\033[38m{}'.format("█")+'\033[30m{}'.format("█")*pole[p][o]
-            elif color == 1:
-                kolco = '\033[31m{}'.format("█")*pole[p][o] + '\033[38m{}'.format("█")+'\033[31m{}'.format("█")*pole[p][o]
-            elif color == 2:
-                kolco = '\033[32m{}'.format("█")*pole[p][o] + '\033[38m{}'.format("█")+'\033[32m{}'.format("█")*pole[p][o]
-            elif color == 3:
-                kolco = '\033[33m{}'.format("█")*pole[p][o] + '\033[38m{}'.format("█")+'\033[33m{}'.format("█")*pole[p][o]
-            elif color == 4:
-                kolco = '\033[34m{}'.format("█")*pole[p][o] + '\033[38m{}'.format("█")+'\033[34m{}'.format("█")*pole[p][o]
-            elif color == 5:
-                kolco = '\033[35m{}'.format("█")*pole[p][o] + '\033[38m{}'.format("█")+'\033[35m{}'.format("█")*pole[p][o]
-            elif color == 6:
-                kolco = '\033[36m{}'.format("█")*pole[p][o] + '\033[38m{}'.format("█")+'\033[36m{}'.format("█")*pole[p][o]
-            elif color == 7:
-                kolco = '\033[37m{}'.format("█")*pole[p][o] + '\033[38m{}'.format("█")+'\033[37m{}'.format("█")*pole[p][o]
-            elif color == 8:
-                kolco = '\033[38m{}'.format("█")*pole[p][o] + '\033[38m{}'.format("█")+'\033[38m{}'.format("█")*pole[p][o]
-            stroka = stroka + (rings - pole[p][o])* " " + kolco + " "*(rings - pole[p][o]) + " " * rings
+    for i in range(rings):
+        stroka = '\033[{}m{}'.format(39," ")
+        for j in range(towers):
+            kolco = '\033[{}m{}'.format(39,"")
+            color1 = pole[j][i]%8+30
+            color2 = pole[j][i]//8+color1
+            if color2>8:
+                color1 = color1+1
+                color2 = pole[j][i]//8+color1
+            if pole[j][i]==15:
+                color2 = 30
+            for k in range(pole[j][i]*2+1):
+                if k==pole[j][i]:
+                    kolco = kolco+'\033[{}m{}'.format(39,"█")
+                elif k%2==0:
+                    kolco = kolco + '\033[{}m{}'.format(color1, "█")
+                else:
+                    kolco = kolco + '\033[{}m{}'.format(color2, "█")
+
+            stroka = stroka + (rings - pole[j][i])* " " + kolco + " "*(rings - pole[j][i]) + " " * rings
         print(stroka)
 kadr()
