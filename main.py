ca56bf478097a5efb7123549e3344c9a4aa81917
sfif7890 <dvornikov.sashakpc1882020@gmail.com>
2022-12-19 03:55:27 +0400

    ready

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import random
+import keyboard
+import time
 
 while True:
     towers = int(input("Введите количество башен  от 3 до 10 "))
@@ -25,12 +27,17 @@ for i in range(towers):
             pole[i].append(j+1)
         else:
             pole[i].append(0)
+for i in range(towers):
+    pole[i].append(0)
+#print(pole)
+
+
+
 
 while True:
     t3 = random.randint(1,towers)
     if t3 != start and t3 != end:
         break
-print("\033[38m{}".format("█"),"\033[0m{}".format("█"),"\033[39m{}".format("█"))
 
 def kadr():
     for i in range(15):
@@ -56,4 +63,56 @@ def kadr():
 
             stroka = stroka + (rings - pole[j][i])* " " + kolco + " "*(rings - pole[j][i]) + " " * rings
         print(stroka)
-kadr()
+
+def hod(position1,position2,colco):
+    while True:
+        if keyboard.is_pressed('enter'):
+            time.sleep(0.1)
+            break
+
+    for i in range(towers):
+        for j in range(rings):
+            if pole[i][j] == colco:
+                pole[i][j] = 0
+                #print(i," ",j)
+                break
+    for i in range(rings+1):
+        #print(position2 - 1, " ", i," ",rings)
+        if (pole[position2-1][i]!=0) or (i == rings):
+            pole[position2-1][i-1] = colco
+
+            break
+
+
+    kadr()
+    #print(position1," ",position2, " ", colco)
+
+for i in range(1,towers+1):
+    #print(i,' ',towers-i)
+    if (rings%2)==0:
+        hod(i-1,i,1)
+    else:
+        hod(towers-i+2,towers-i+1,1)
+
+
+def moves(n, x=1, y=3):
+    if x == 1:
+        position1 = start
+    elif x == 2:
+        position1 = t3
+    else:
+        position1 = end
+    if y == 1:
+        position2 = start
+    elif y == 2:
+        position2 = t3
+    else:
+        position2 = end
+    if n == 1:
+        hod(position1,position2,n)
+    else:
+        moves(n - 1, x, 6 - x - y)
+        hod(position1, position2,n)
+        moves(n - 1, 6 - x - y, y)
+
+moves(rings)
